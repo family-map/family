838f19d509f03f14075a498e2008f77c58a2bb68
GXY <ggg_xiaoyu@163.com>
2020-05-17 19:46:58 +0800

    家族主要业务服务

--- fml-eureka-server/src/main/java/com/family/fmleurekaserver/FmlEurekaServerApplication.java
+++ fml-eureka-server/src/main/java/com/family/fmleurekaserver/FmlEurekaServerApplication.java
@@ -2,8 +2,12 @@ package com.family.fmleurekaserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.context.annotation.ComponentScan;
 
+@EnableEurekaServer
 @SpringBootApplication
+@ComponentScan("com.family")
 public class FmlEurekaServerApplication {
 
     public static void main(String[] args) {
