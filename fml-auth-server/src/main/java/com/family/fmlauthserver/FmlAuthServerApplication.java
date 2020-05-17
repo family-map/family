55373f31723cfdccc588b0e7b27476ac97b401a0
GXY <ggg_xiaoyu@163.com>
2020-05-17 16:24:00 +0800

    授权中心提交

---
+++ fml-auth-server/src/main/java/com/family/fmlauthserver/FmlAuthServerApplication.java
@@ -0,0 +1,13 @@
+package com.family.fmlauthserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FmlAuthServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FmlAuthServerApplication.class, args);
+    }
+
+}
---
+++ fml-auth-server/src/test/java/com/family/fmlauthserver/FmlAuthServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.family.fmlauthserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FmlAuthServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
