d498062d9e00b6faaee82aa61802a5f6bde8cc5e
GXY <ggg_xiaoyu@163.com>
2020-05-17 16:31:21 +0800

    家族主要业务服务提交

---
+++ fml-genealogy/src/main/java/com/family/fmlgenealogy/FmlGenealogyApplication.java
@@ -0,0 +1,13 @@
+package com.family.fmlgenealogy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FmlGenealogyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FmlGenealogyApplication.class, args);
+    }
+
+}
---
+++ fml-genealogy/src/test/java/com/family/fmlgenealogy/FmlGenealogyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.family.fmlgenealogy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FmlGenealogyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
