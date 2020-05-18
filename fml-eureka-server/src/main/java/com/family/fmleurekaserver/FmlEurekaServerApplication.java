0890831e0e8468dd0dfb168ad9c35eadf209d933
GXY <ggg_xiaoyu@163.com>
2020-05-18 16:21:05 +0800

    家族主要业务服务

--- fml-eureka-server/src/main/java/com/family/fmleurekaserver/FmlEurekaServerApplication.java
+++ fml-eureka-server/src/main/java/com/family/fmleurekaserver/FmlEurekaServerApplication.java
@@ -2,11 +2,13 @@ package com.family.fmleurekaserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 import org.springframework.context.annotation.ComponentScan;
 
 @EnableEurekaServer
-@SpringBootApplication
+@SpringBootApplication(exclude={DataSourceAutoConfiguration.class, HibernateJpaAutoConfiguration.class})
 @ComponentScan("com.family")
 public class FmlEurekaServerApplication {
 
--- fml-gateway-server/src/main/java/com/family/fmlgatewayserver/FmlGatewayServerApplication.java
+++ fml-gateway-server/src/main/java/com/family/fmlgatewayserver/FmlGatewayServerApplication.java
@@ -2,10 +2,12 @@ package com.family.fmlgatewayserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @EnableDiscoveryClient
-@SpringBootApplication
+@SpringBootApplication(exclude={DataSourceAutoConfiguration.class, HibernateJpaAutoConfiguration.class})
 public class FmlGatewayServerApplication {
 
     public static void main(String[] args) {
