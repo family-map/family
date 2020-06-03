a79bd61a27020306b804c978bee19bd52a99198b
guoxiaoyu <123QWEqwe>
2020-06-03 14:07:12 +0800

    更新

--- fml-auth-server/src/main/java/com/family/fmlauthserver/FmlAuthServerApplication.java
+++ fml-auth-server/src/main/java/com/family/fmlauthserver/FmlAuthServerApplication.java
@@ -2,8 +2,12 @@ package com.family.fmlauthserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 
-@SpringBootApplication
+@SpringBootApplication(exclude={DataSourceAutoConfiguration.class, HibernateJpaAutoConfiguration.class})
+@EnableAuthorizationServer
 public class FmlAuthServerApplication {
 
     public static void main(String[] args) {
